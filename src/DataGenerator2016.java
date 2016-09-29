d665fbf1bee31797bb7579ac3009a1bb8970232b
elenam <elenam@morris.umn.edu>
2016-09-28 19:22:20 -0500

    generating random walks

--- src/DataGenerator2016.java
+++ src/DataGenerator2016.java
@@ -1,4 +1,3 @@
-
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.Random;
@@ -9,45 +8,82 @@ public class DataGenerator2016 {
 	 * being written to the file given as the first command line argument.
 	 * 
 	 * @param args
-	 *           
+	 * 
 	 * 
 	 * 
 	 *            Author: Elena Machkasova
 	 */
-	
-	static int timestamp = 0; 
 
-	public static void main(String [] args) {
+	static int timestamp = 0;
+
+	public static void main(String[] args) {
 		int defaultGridSide = 100000;
 		int defaultN = 10000000;
-		int defaultWalkMin = 100;
-		int defaultWalkMax = 1000;
-		int defaultMaxSpeed = 100; 
-		
-		
+		int defaultWalkMin = 10;
+		int defaultWalkMax =  500;
+		int defaultMaxSpeed = 100;
+
 		int gridSide = defaultGridSide;
 		int n = defaultN;
 		int walkMin = defaultWalkMin;
 		int walkMax = defaultWalkMax;
-		// the angle is always changed between 0 and 360
+		int maxSpeed = defaultMaxSpeed;
+		// the angle is always between 0 and 360
 		
-		
-		
-		// start at the center 
+		int walks = 0;
+
+		// pick two points at random
+
+		// start at the center
 		int startX = gridSide / 2;
 		int startY = gridSide / 2;
+
+		int[][] points = new int[n][3];
+		points[0][0] = startX;
+		points[0][1] = startY;
+		points[0][2] = timestamp++;
+
+		// keep generating walks until the timestamp is over the needed number
+		// of points
+		while (timestamp < n) {
+			// generate length of a walk
+			int length = defaultWalkMin
+					+ (int) (Math.random() * (defaultWalkMax - defaultWalkMin));
+			// make sure we are not going over n:
+			if (length + timestamp > n)
+				length = n - timestamp;
+			// store the current number of points for copying
+			int oldLength = timestamp;
+			
+			// generate a direction
+			double angle = Math.random() * 360;
+			double direction = Math.toRadians(angle);
+
+			// generate speed
+			int speed = (int) (Math.random() * maxSpeed);
+
+			int[][] walkPoints = walk(points[timestamp - 1][0],
+					points[timestamp - 1][1], direction, speed, length);
+			
+			// copy walk points into points
+			for (int i = 0; i < length; ++i) {
+				points[oldLength + i][0] = walkPoints[i][0]; // copy x
+				points[oldLength + i][1] = walkPoints[i][1]; // copy y
+				points[oldLength + i][2] = walkPoints[i][2]; // copy timestamp
+			}
+			
+			System.out.println("The walk at the angle " + angle + "at the speed " + speed + " ends at point " + timestamp);
+			++walks;
+		}
+		//printArray(points);
 		
-		// generate a direction
-		// generate number of points
-		// generate speed
+		//System.out.println("The total number of walks is " + walks);
 		
-		// keep generating walks until the timestamp is over the needed number of points
-		
-		
+
 		// Test prints:
 		//int[][] points = walk(50, 50, Math.toRadians(90), 5, 100);
-		int[][] points = walk(50, 50, Math.toRadians(0), 5, 100);
-		printArray(points);
+		//int[][] points = walk(50, 50, Math.toRadians(0), 5, 100);
+		//printArray(points);
 		
 		
 	}
@@ -56,17 +92,17 @@ public class DataGenerator2016 {
 	 * direction: heading direction, in radians 
 	 */
 	private static int [][] walk(int xStart, int yStart, double direction, int speed, int n) {
-		int [][] points = new int[n][3];
+		int [][] walkPoints = new int[n][3];
 		// the first point is based on the start point:
-		points[0][0] = generateX(xStart, direction, speed);
-		points[0][1] = generateY(yStart, direction, speed);
-		points[0][2] = timestamp++;
+		walkPoints[0][0] = generateX(xStart, direction, speed);
+		walkPoints[0][1] = generateY(yStart, direction, speed);
+		walkPoints[0][2] = timestamp++;
 		for (int i = 1; i < n; ++i) {
-			points[i][0] = generateX(points[i-1][0], direction, speed);
-			points[i][1] = generateY(points[i-1][1], direction, speed);
-			points[i][2] = timestamp++;
+			walkPoints[i][0] = generateX(walkPoints[i-1][0], direction, speed);
+			walkPoints[i][1] = generateY(walkPoints[i-1][1], direction, speed);
+			walkPoints[i][2] = timestamp++;
 		}
-		return points;
+		return walkPoints;
 	}
 	
 	private static int generateX(int x, double direction, int speed) {
